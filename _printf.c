35c81004ae93fdf3774a86fdccf2626f11d11f02
Qonda Mampofu <mampofuq@gmail.com>
2023-08-20 13:18:18 -0700

    output according to format

--- _printf.c
+++ _printf.c
@@ -6,15 +6,43 @@
  */
 int _printf(const char *format, ...)
 {
-	int i = 0;
+	int count = 0, i = 0, chr, j;
+	char *str;
 	va_list arg;
 
 	va_start(arg, format);
 	while (format[i] != '\0')
 	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					chr = va_arg(arg, int);
+					_putchar(chr);
+					i++;
+					count++;
+					break;
+				case 's':
+					str = va_arg(arg, char *);
+					for (j = 1; str[j] != '\0'; j++)
+						count++;
+					_puts(str);
+					i++;
+					count++;
+					break;
+				case '%':
+					_putchar('%');
+					i++;
+					count++;
+					break;
+			}
+		}
 		_putchar(format[i]);
+		count++;
 		i++;
 	}
-	return (i);
+	return (count);
 }
 
--- main.c
+++ main.c
@@ -9,9 +9,19 @@
  */
 int main(void)
 {
+	int len, len1;
 
 	printf("Let's try to printf a simple sentence.\n");
 	_printf("let's try to printf a simple sentence.\n");
 
+	printf("Character:[%c]\n", 'H');
+	_printf("Character:[%c]\n", 'H');
+
+	printf("String:[%s]\n", "I am a string !");
+	_printf("String:[%s]\n", "I am a string !");
+
+	len = printf("Percent:[%%]\n");
+	len1 = _printf("Percent:[%%]\n");
+	printf("len: %d\nlen1: %d\n", len, len1);
 	return (0);
 }
