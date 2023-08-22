4f4e0dfb158a01c5f419440f1e6b817322b522d6
Qonda Mampofu <mampofuq@gmail.com>
2023-08-22 04:59:11 -0700

    edge cases

--- _printf.c
+++ _printf.c
@@ -18,7 +18,7 @@ int _printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			i++;
-			if (format[i] == '\0' || format[1] == ' ')
+			if (format[i] == '\0')
 			{
 				va_end(arg);
 				return (-1);
